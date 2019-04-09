9e5a85581af81cf2080f60b7865bcb2f862c9e90
LaDimas <la_dimas@mail.ru>
2019-04-09 19:41:10 +0600

    Commit

---
+++ src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите полный путь к файлу архива");
+        String address = reader.readLine();
+        Path fileZip = Paths.get(address);
+        ZipFileManager zipFile = new ZipFileManager(fileZip);
+        System.out.println("Введите полный путь к файлу для архивирования");
+        String file = reader.readLine();
+        Path fileToZip = Paths.get(file);
+        zipFile.createZip(fileToZip);
+    }
+}
