c85a60bc8151c62987ac4701dea9e065870abc1a
LaDimas <gaydukov.foto@gmail.com>
2019-07-29 22:15:45 +0500

    Commit

--- src/com/javarush/task/task27/task2707/Solution.java
+++ src/com/javarush/task/task27/task2707/Solution.java
@@ -1,52 +1,40 @@
 package com.javarush.task.task27.task2707;
 
-import static java.lang.Thread.sleep;
-
-/*
-Определяем порядок захвата монитора
-*/
 public class Solution {
-    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
-        synchronized (obj1) {
-            synchronized (obj2) {
-                System.out.println(obj1 + " " + obj2);
-            }
-        }
+    private final String field;
+
+    public Solution(String field) {
+        this.field = field;
     }
 
-    public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
-
-        Thread thread1 = new Thread() {
-            @Override
-            public void run() {
-                synchronized (o1) {
-                    try {
-                        sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    synchronized (o2) {
-                    }
-                }
-            }
-        };
-        Thread thread2 = new Thread() {
-            @Override
-            public void run() {
-                solution.someMethodWithSynchronizedBlocks(o1, o2);
-            }
-        };
-        thread1.start();
-        thread2.start();
-        sleep(500);
-        System.out.println(thread2.getState());
-        return (!Thread.State.BLOCKED.equals(thread2.getState()));
+    public String getField() {
+        return field;
     }
-    public static void main(String[] args) throws Exception {
-        final Solution solution = new Solution();
-        final Object o1 = new Object();
-        final Object o2 = new Object();
 
-        System.out.println(isNormalLockOrder(solution, o1, o2));
+    public synchronized void sout(Solution solution) {
+        System.out.format("111:  %s: %s %n", this.field, solution.getField());
+        solution.sout2(this);
+    }
+
+    public synchronized void sout2(Solution solution) {
+        System.out.format("222:  %s: %s %n", this.field, solution.getField());
+        solution.sout(this);
+    }
+
+    public static void main(String[] args) {
+        final Solution solution = new Solution("first");
+        final Solution solution2 = new Solution("second");
+        new Thread(new Runnable() {
+            public void run() {
+                solution.sout(solution2);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                solution2.sout(solution);
+            }
+        }).start();
+
     }
 }
